00bc52af9a9c866c31179784ec118f63444ce0fd
fluttercount <fluttercount8@gmail.com>
2025-03-15 00:43:31 +0000

    finalizado

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,122 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBRcuQ8vTOD63sjJmR7wI_1xSrDvyxud3o",
+      appId: "1:1013796660455:android:bdb4990749b81e12b3ae42",
+      messagingSenderId: "1013796660455",
+      projectId: "projeto-conversor-9e453",
+      databaseURL:
+          "https://projeto-conversor-9e453-default-rtdb.firebaseio.com",
+      storageBucket: "projeto-conversor-9e453.firebasestorage.app",
+    ),
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Conversor de Moeda',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ConversaoTela(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ConversaoTela extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ConversaoTelaState createState() => _ConversaoTelaState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ConversaoTelaState extends State<ConversaoTela> {
+  final _valorController = TextEditingController();
+  String? _moedaOrigem = 'USD';
+  String? _moedaDestino = 'BRL';
+  String? _resultado;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final DatabaseReference _database = FirebaseDatabase.instance.ref();
+
+  List<String> _moedas = ['USD', 'BRL', 'EUR', 'GBP'];
+  Future<void> _conversao() async {
+    if (_valorController.text.isNotEmpty) {
+      double valor = double.tryParse(_valorController.text) ?? 0.0;
+
+      final taxas = await _database.child('taxas').get();
+      var taxasMap = taxas.value as Map<dynamic, dynamic>?;
+
+      if (taxasMap != null) {
+        var taxaOrigemDestino = taxasMap[_moedaOrigem]?[_moedaDestino] ?? 0.0;
+
+        setState(() {
+          _resultado = (valor * taxaOrigemDestino).toStringAsFixed(2);
+        });
+      } else {
+        setState(() {
+          _resultado = 'Erro ao carregar taxas';
+        });
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: Text('Conversão de Moeda')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            TextField(
+              controller: _valorController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Valor'),
             ),
+            SizedBox(height: 16),
+            DropdownButton<String>(
+              value: _moedaOrigem,
+              onChanged: (value) {
+                setState(() {
+                  _moedaOrigem = value;
+                });
+              },
+              items:
+                  _moedas.map((moeda) {
+                    return DropdownMenuItem<String>(
+                      value: moeda,
+                      child: Text(moeda),
+                    );
+                  }).toList(),
+            ),
+            SizedBox(height: 16),
+            DropdownButton<String>(
+              value: _moedaDestino,
+              onChanged: (value) {
+                setState(() {
+                  _moedaDestino = value;
+                });
+              },
+              items:
+                  _moedas.map((moeda) {
+                    return DropdownMenuItem<String>(
+                      value: moeda,
+                      child: Text(moeda),
+                    );
+                  }).toList(),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(onPressed: _conversao, child: Text('Converter')),
+            SizedBox(height: 16),
+            if (_resultado != null) Text('Resultado: $_resultado'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
